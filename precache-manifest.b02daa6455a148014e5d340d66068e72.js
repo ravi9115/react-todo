58894c644beba22f2eb7f292ac44039abd4cc96a
Ravi J <ravi9115ravi@gmail.com>
2020-06-05 18:17:56 +0530

    Updates

---
+++ precache-manifest.b02daa6455a148014e5d340d66068e72.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "759976af4342f8da7ae56591586a1a58",
+    "url": "/static/media/OS.759976af.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "a47befa7372f6aa93e4b",
+    "url": "/static/js/main.a47befa7.chunk.js"
+  },
+  {
+    "revision": "87bce6678be0181d62f3",
+    "url": "/static/js/2.87bce667.chunk.js"
+  },
+  {
+    "revision": "a47befa7372f6aa93e4b",
+    "url": "/static/css/main.614de976.chunk.css"
+  },
+  {
+    "revision": "ea384fd50899dbe8d7e12bca5e44f6c9",
+    "url": "/index.html"
+  }
+];
